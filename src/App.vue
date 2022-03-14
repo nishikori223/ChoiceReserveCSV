78ddc6643a4f9d2346d495aca66b9a1bb32d3607
堤 祐次朗 <tsutsumi.y@nds-tyo.co.jp>
2022-03-14 23:05:15 +0900

    デザイン修正

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
+    <img class="mb-4 mr-3" style="width: 50px; height: 75px" src="./assets/noa.png" />
     <img class="mb-4" src="./assets/logo.png" />
+    <img class="mb-4" style="width: 70px; height: 70px" src="./assets/chinu.png" />
     <csvAnalysis />
   </div>
 </template>
